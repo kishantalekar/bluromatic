f9470452b506d7e76220fd17b9f7987839b94a17
kishantalekar <kishantalekar024@gmail.com>
2024-07-16 00:37:36 +0530

    done

---
+++ app/src/main/java/com/example/bluromatic/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.bluromatic
+
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bluromatic.ui.BluromaticScreen
+import com.example.bluromatic.ui.theme.BluromaticTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BluromaticTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                  BluromaticScreen()
+                }
+            }
+        }
+    }
+}
+
+fun Context.getImageUri(): Uri {
+    val resources = this.resources
+
+    return Uri.Builder()
+        .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+        .authority(resources.getResourcePackageName(R.drawable.android_cupcake))
+        .appendPath(resources.getResourceTypeName(R.drawable.android_cupcake))
+        .appendPath(resources.getResourceEntryName(R.drawable.android_cupcake))
+        .build()
+}
---
+++ app/src/main/java/com/example/bluromatic/data/BlurAmount.kt
@@ -0,0 +1,5 @@
+package com.example.bluromatic.data
+
+import androidx.annotation.StringRes
+
+data class BlurAmount (@StringRes val blurAmountRes: Int,val blurAmount:Int)
